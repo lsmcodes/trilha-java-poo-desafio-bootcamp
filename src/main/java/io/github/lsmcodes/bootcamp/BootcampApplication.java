0408d5780a8376e3c3ea49059b56eb3be350f800
Lorena Medeiros <lomedeiros@proton.me>
2024-06-05 02:55:35 -0300

    feat: invoke mostrarMenu method from the Catalogo instance in the main class

--- src/main/java/io/github/lsmcodes/bootcamp/BootcampApplication.java
+++ src/main/java/io/github/lsmcodes/bootcamp/BootcampApplication.java
@@ -3,11 +3,16 @@ package io.github.lsmcodes.bootcamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.lsmcodes.bootcamp.model.catalogo.Catalogo;
+
 @SpringBootApplication
 public class BootcampApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
+
+		Catalogo catalogo = new Catalogo();
+		catalogo.mostrarMenu();
 	}
 
 }
